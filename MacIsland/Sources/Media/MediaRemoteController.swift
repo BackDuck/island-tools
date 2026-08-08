45bc870c8371d1c6bd0bb162e5b600d482af52c7
Nurshat Nizamov <q7ace@ya.ru>
2026-08-08 16:39:40 +0300

    feat: add sources batch 11

---
+++ MacIsland/Sources/Media/MediaRemoteController.swift
@@ -0,0 +1,14 @@
+import Foundation
+import AppKit
+import Darwin
+
+/// stub placeholder - will be replaced
+final class MediaRemoteController {
+    var onUpdate: ((NowPlayingInfo) -> Void)?
+    func start() {}
+    func stop() {}
+    func togglePlayPause() {}
+    func nextTrack() {}
+    func previousTrack() {}
+    func seek(to fraction: Double) {}
+}
